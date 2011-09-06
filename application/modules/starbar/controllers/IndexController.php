c8e632ab866ff77bf6ff1ff3806965de6054a644
davidbjames <davidbjames@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-09-06 13:01:35 +0000

    - re-arranged JS/CSS includes for HelloMusic Starbar to better fit browser app architecture - enabled starbar/index/hello-music-remote (and hooked that into they Kynetx app itself) - updated latest KRL for Starbar

--- application/modules/starbar/controllers/IndexController.php
+++ application/modules/starbar/controllers/IndexController.php
@@ -42,16 +42,19 @@ class Starbar_IndexController extends Api_AbstractController
     }
     
     public function hellomusicAction () {
+        // the following css/js is included here (and not in the remote view) to mimic the Kynetx app which loads
+        // these dynamically *ahead* of the ajax call (which pulls in the markup and the rest of the css/js)
+        
 				//$this->view->headLink()->appendStylesheet('/css/smoothness/jquery-ui-1.8.16.custom.css');
-				$this->view->headLink()->appendStylesheet('/css/jquery.jscrollpane.css');
         $this->view->headLink()->appendStylesheet('/css/starbar-generic.css');
         $this->view->headLink()->appendStylesheet('/css/starbar-hellomusic.css');
         $this->view->headScript()->appendFile('/js/starbar/jquery-1.6.1.min.js');
-        $this->view->headScript()->appendFile('/js/starbar/jquery-ui-1.8.16.custom.min.js');
     }
     
     public function hellomusicRemoteAction () {
-        
+        $this->_enableRenderer(new Api_Plugin_JsonPRenderer());
+        $this->render();
+        return $this->_resultType(new Object(array('html' => $this->getResponse()->getBody())));
     }
 }
 

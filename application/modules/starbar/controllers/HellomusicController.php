c30c06cb056e28b5c3e396faf2f1fb5ffd9b3b8a
hamzakubba <hamza@say.so>
2011-09-19 21:30:48 +0000

    Daily deals now uses XML feed (working prelim. version)

--- application/modules/starbar/controllers/HellomusicController.php
+++ application/modules/starbar/controllers/HellomusicController.php
@@ -15,6 +15,13 @@ class Starbar_HellomusicController extends Starbar_ContentController
 	// Daily deals is probably unique to each starbar
     public function dailyDealsAction ()
     {
+		$feedUrl = "http://staging.hellomusic.com/ec/Interpret.aspx?auth=uyskCsCO5jeS2d1fc5";
+		$xml = simpleXML_load_file($feedUrl, "SimpleXMLElement", LIBXML_NOCDATA);
 
+		if($xml ===  FALSE) {
+
+		} else {
+			$this->view->assign('deals', $xml);
+		}
     }
 }

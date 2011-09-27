27bfe8985dd833da4030f0f1a42e89ff27394eac
davidbjames <davidbjames@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-09-27 15:50:39 +0000

    - incorporated ALL pre-install onboarding Javascript in one file to be delivered to HelloMusic (and then deleted /js/starbar/pre-install.js) - created testing version of loggedIn page at /client/hellomusic/loggedIn.php. This includes creating the HelloMusic cookies (via random email)

--- public/client/hellomusic/js/starbar-onboard.js
+++ public/client/hellomusic/js/starbar-onboard.js
@@ -16,24 +16,35 @@
     if (!window.sayso.baseDomain) window.sayso.baseDomain = 'local.sayso.com';
     
     var sayso = window.sayso;
-    
-    /**
-     * Javascript Load Timer
-     * @author davidbjames
-     */
-    function jsLoadTimer(){function d(){window.hasOwnProperty(e)?a():b++>c?a():setTimeout(d,50)}var b=0,c=200,e="",a=null;this.start=function(b,c){e=b;a=c;d()}};
 
-    var jQuery = document.createElement('script'); 
-    jQuery.src = 'http://' + sayso.baseDomain + '/js/starbar/jquery-1.6.1.min.js';
-    document.getElementsByTagName('body')[0].appendChild(jQuery);
+    var jQueryInclude = document.createElement('script'); 
+    jQueryInclude.src = 'http://' + sayso.baseDomain + '/js/starbar/jquery-1.6.1.min.js';
+    document.getElementsByTagName('body')[0].appendChild(jQueryInclude);
     
     var loadTimer = new jsLoadTimer();
     loadTimer.start('$S', function () {
+        
+        // -------------------------------------------
+        // Custom setup 
         
         var elemPage = $S('#sayso-onboard');
 
         elemPage.height($S(window).height());
         elemPage.width($S(window).width());
+        
+        $S('#sso_wrapper input[type=radio]').attr('checked', false);
+        
+        $S('#sayso-get-app').click(function(e) {
+            if (!$S('#sso_wrapper input[type=radio]').is(':checked')) {
+                e.preventDefault();
+                $S('span.sso_textError').fadeIn('slow');
+            }
+        });
+        $S('#sso_wrapper input[type=radio]').bind('change', function () {
+            $S('span.sso_textError').fadeOut('slow');
+        });
+        
+        // -------------------------------------------
 
         String.prototype.trim = function() {
             return this.replace(/^\s+|\s+$/g,'');
@@ -53,9 +64,11 @@
                 return '';
             }
             
+            // -------------------------------------------
+            // Custom setup 
+            
             var loginCookie = getCookie('CHOMPUID'),
                 userEmail = getCookie('MyEmail');
-        
             
             sayso.client = {
                 name : 'hellomusic',
@@ -63,6 +76,8 @@
                 uuidType : 'email',
                 userLoggedIn : false
             };
+            
+            // -------------------------------------------
             
             if (loginCookie && userEmail) {
                 sayso.client.uuid = userEmail;
@@ -72,13 +87,60 @@
             window.saysoClientSetup = true;
         }
         
-        var loadTimer2 = new jsLoadTimer();
-        loadTimer2.start('saysoClientSetup', function () {
+        var loginTimer = new jsLoadTimer();
+        loginTimer.start('saysoClientSetup', function () {
             if (sayso.client.userLoggedIn) {
-                var preInstall = document.createElement('script'); 
-                preInstall.src = 'http://' + sayso.baseDomain + '/js/starbar/pre-install.js';
-                document.getElementsByTagName('body')[0].appendChild(preInstall);
+                
+                // detect browser and provide appropriate install link
+                var browserAppUrl = 'http://' + sayso.baseDomain + '/install';
+                
+                if (navigator.userAgent.match('Firefox')) {
+                    browserAppUrl += '/firefox/SaySo-DEV.xpi';
+                } else if (navigator.userAgent.match('Chrome')) {
+                    browserAppUrl += '/chrome/SaySo-DEV.crx';
+                } else if (navigator.userAgent.match('MSIE')) {
+                    browserAppUrl += '/ie/SaySo-DEV-Setup.exe';
+                } else if (navigator.userAgent.match('Apple')) {
+                    browserAppUrl += '/safari/SaySo-DEV.hmm';
+                } else {
+                    // Browser is not supported. Must be Firefox, Chrome, Safari or Internet Explorer.
+                    return;
+                }
+                
+                $S('#sayso-get-app').attr('href', browserAppUrl);
+
+                // Pre-Install routine
+                // pass uuid and token for this user to sayso
+                // and in return set cookies on the client
+                
+                var iframe = document.createElement('iframe');
+                iframe.src = 'http://' + sayso.baseDomain + '/starbar/remote/pre-install?auth_key=309e34632c2ca9cd5edaf2388f5fa3db&client_name=' + sayso.client.name + '&client_uuid=' + sayso.client.uuid + '&client_uuid_type=' + sayso.client.uuidType + '&client_user_logged_in=' + (sayso.client.userLoggedIn ? 'true' : '') + '&install_token=' + getRandomToken();
+                iframe.width= '0'; iframe.height = '0'; 
+                iframe.scrolling='0'; iframe.style = 'width: 0; height: 0; border: none; display: none;';
+                
+                document.getElementsByTagName('body')[0].appendChild(iframe);
+                
+                
             }
         });
     });
+    
+    /**
+     * Javascript Load Timer
+     * @author davidbjames
+     */
+    function jsLoadTimer(){function d(){window.hasOwnProperty(e)?a():b++>c?a():setTimeout(d,50)}var b=0,c=200,e="",a=null;this.start=function(b,c){e=b;a=c;d()}};
+    
+    /**
+     * Get a random 64 character token 
+     */
+    function getRandomToken() {
+        
+        var s = [],
+            characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (var i = 0; i < 64; i++) {
+            s[i] = characters.substr(Math.floor(Math.random() * 36), 1);
+        }
+        return s.join('');
+    }
 })();
---
+++ public/client/hellomusic/loggedIn.php
@@ -0,0 +1,50 @@
+<?php
+
+if (!isset($_COOKIE['CHOMPUID']) && !isset($_COOKIE['MyEmail'])) {
+    $randomEmail = substr(str_shuffle('bcdfghjklmnpqrstvwxz'), 0, 1) . substr(str_shuffle('aeiouy'), 0, 1) . substr(str_shuffle('bcdfghjklmnpqrstvwxz'), 0, 2) . substr(str_shuffle('aeiouy'), 0, 1) . substr(str_shuffle('bcdfghjklmnpqrstvwxz'), 0, 1) . substr(str_shuffle('123456789'), 0, 1);
+    $randomEmail .= '@go.com';
+    setcookie('CHOMPUID', md5($randomEmail));
+    setcookie('MyEmail', $randomEmail);
+} else {
+    $randomEmail = $_COOKIE['MyEmail'];
+}
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/sayso-onboard.css" />
+</head>
+<body style="margin: 0; padding: 0;">
+    <div id="sayso-onboard">
+    	<div id="sso_wrapper">
+          	<div id="sso_logo">
+            	<a href=""><img src="images/logo_hello-music.png" alt="HelloMusic&trade; Logo" /></a>
+            </div><!-- #sso_logo -->
+          	<div class="sso_content">
+                <h1>Keep The Beat!<br />With the BeatBar</h1>
+                <h4>You've been selected as a member of the rhythm<br />
+                section keeping HelloMusic products and deals in<br />
+                sync with you, our customers.</h4>
+                <h3><span class="sso_textHighlight">GRAB</span> the BeatBar. <span class="sso_textHighlight">GIVE</span> your opinion.</h3>
+                <h2><span class="sso_textHighlight">GET</span> FREE GEAR.</h2>
+                <h4 style="height: 50px;">
+                <span class="sso_textError" style="display: none;">Whoops. Make sure you agree to the terms<br />
+                and conditions before you download.</span>
+                </h4>
+                <form action="" name="" method="">
+                    <p><input type="radio" /> I agree to the <a href="">terms</a> and <a href="">conditions</a></p>
+                    <!--<p><input id="sayso-get-app" type="submit" value="GRAB IT" class="sso_theme_button sso_theme_buttonXL" /></p>
+                    --><p><a id="sayso-get-app" href="" class="sso_theme_button sso_theme_buttonXL">GRAB IT</a></p>
+                </form>
+            </div><!-- .sso_content -->
+            <div class="sso_main-image"></div><!-- .sso_main-image -->
+        </div><!-- #sso_wraper -->
+        <script type="text/javascript" src="js/starbar-onboard.js"></script>
+    </div><!-- #sayso-onboard -->
+    <div style="position: fixed; bottom: 4px; left: 4px; font-weigth: bold; color: white; z-index: 99999; font-family: Verdana;"><?= $randomEmail ?></div>
+</body>
+</html>

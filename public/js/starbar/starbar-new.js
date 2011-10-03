f0bbd903828c3528ca71305781330eafc1c7af81
hamzakubba <hamza@say.so>
2011-10-03 00:58:00 +0000

    More testing with starbar-new.js

--- public/js/starbar/starbar-new.js
+++ public/js/starbar/starbar-new.js
@@ -11,7 +11,12 @@ $SQ.ajaxWithAuth = function (options) {
     try
     {
     	sayso = window.sayso; // should work for starbar itself
-    	sayso = top.window.sayso; // should work in an iframe
+	}
+	catch (e) {}
+    
+    try
+    {
+    	sayso = window.opener.sayso; // should work in popups (ones opened with window.open)
 	}
 	catch (e) {}
     
@@ -72,7 +77,12 @@ $SQ(function(){
     try
     {
     	sayso = window.sayso; // should work for starbar itself
-    	sayso = top.window.sayso; // should work in an iframe
+	}
+	catch (e) {}
+    
+    try
+    {
+    	sayso = window.opener.sayso; // should work in popups (ones opened with window.open)
 	}
 	catch (e) {}
     

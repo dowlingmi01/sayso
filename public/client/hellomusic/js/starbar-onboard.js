267ca92ffc1d46bd1a6addfdae3def6c0a913783
joanna.weinman <joanna.weinman@gmail.com>
2011-10-11 17:11:48 +0000

    REWARDS CENTER - minor style fixes.

--- public/client/hellomusic/js/starbar-onboard.js
+++ public/client/hellomusic/js/starbar-onboard.js
@@ -39,8 +39,9 @@
         elemPage.width($SQ(window).width());
         
 				$SQ(window).resize(function() {
-					elemOverlay.css('marginLeft','-300px');
+					console.log('boo');
 					elemOverlay.css('left','50%');
+					elemOverlay.css('margin-left','-300px');
 				});
 				
         $SQ('#sso_wrapper input[type=radio]').attr('checked', false);

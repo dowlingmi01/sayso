6e665e2d734ddac8d73d5553151fbf2322c8d97a
davidbjames <davidbjames@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-10-20 15:59:50 +0000

    Added Registry::getPseudoEnvironmentName

--- library/Registry.php
+++ library/Registry.php
@@ -9,5 +9,28 @@ class Registry extends Api_Registry
     public static function getStarbar () {
         return Api_Registry::_get('starbar');
     }
+    
+    /**
+     * Get the environment variable name modified
+     * for external use (app install, naming, etc)
+     * 
+     * @return string
+     */
+    public static function getPseudoEnvironmentName () {
+        switch (APPLICATION_ENV) {
+            case 'development' :
+                $env = 'LOCAL'; break;
+            case 'sandbox' :
+                $env = 'DEV'; break;
+            case 'testing' :
+                $env = 'TEST'; break;
+            case 'staging' :
+                $env = 'STAGE'; break;
+            case 'production' :
+            default :
+                $env = 'PROD'; break;
+        }
+        return $env;
+    }
 }
 

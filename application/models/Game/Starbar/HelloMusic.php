4b1cf0e1b1a4541aef6ac37214399aa8666b90e6
davidbjames <davidbjames@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-10-25 19:28:17 +0000

    Fixed logic in Game_Starbar_HelloMusic to  lock items in Reward center based on LEVEL (busker) and POINTS

--- application/models/Game/Starbar/HelloMusic.php
+++ application/models/Game/Starbar/HelloMusic.php
@@ -18,11 +18,17 @@ class Game_Starbar_HelloMusic extends Game_Starbar {
      */
     protected function _visitGood (Gaming_BigDoor_Good $good)
     {
+        static $buskerLevel = null;
+        if (!$buskerLevel) {
+            $buskerLevel = $this->getLevels()->find('title', 'busker')->getFirst();
+        }
         $profile = $this->getGamer();
         if ($good->getId() !== $this->_economy->getGoodId('WEEK_ONE_GIVEAWAY')) {
             $currencyPrimarySurveyId = $this->_economy->getCurrencyId('PRIMARY_SURVEY_POINTS');
             $currencyPrimarySurvey = $profile->getCurrencies()->find('id', $currencyPrimarySurveyId)->getFirst();
-            if ((int) $currencyPrimarySurvey->current_balance < 2) {
+            $currentLevel = $profile->getHighestLevel();
+            if ((int) $currencyPrimarySurvey->current_balance < 2 || 
+                $currentLevel->ordinal < $buskerLevel->ordinal) {
                 $good->isRedeemable(false);
             }
         } 

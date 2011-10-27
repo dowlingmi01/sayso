3d21af77a7a80257bb74a09f3526085442fd45dd
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-10-27 13:28:34 +0000

    #152 - fix coding rules

--- scripts/sql/2011-10-27-01-session_handler_table.sql
+++ scripts/sql/2011-10-27-01-session_handler_table.sql
@@ -1,6 +1,6 @@
 SET foreign_key_checks = 0;
 
-DROP TABLE IF EXISTS `session`;
+DROP TABLE IF EXISTS session;
 
 /**
  * Use ENGINE=MyISAM to boost up speed
@@ -8,13 +8,13 @@ DROP TABLE IF EXISTS `session`;
  * the charset must NOT be utf8 for the id field
 */
 
-CREATE TABLE IF NOT EXISTS `session` (
-  `id` CHAR(32) CHARACTER SET ascii NOT NULL DEFAULT '',
-  `modified` INT(11) DEFAULT NULL,
-  `lifetime` INT(11) DEFAULT NULL,
-  `data` TEXT,
-  PRIMARY KEY (`id`),
-  KEY `modified` (`modified`)
+CREATE TABLE IF NOT EXISTS session (
+  id CHAR(32) CHARACTER SET ascii NOT NULL DEFAULT '',
+  modified INT(11) DEFAULT NULL,
+  lifetime INT(11) DEFAULT NULL,
+  data TEXT,
+  PRIMARY KEY (id),
+  KEY modified (modified)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 SET foreign_key_checks = 1;

3c0f0c93912722b3066b19429a6b7036906edcf5
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-01 14:49:24 +0000

    #170 	API controller/actions to retrieve metrics - data model mockup

--- application/modules/admin/controllers/MetricsController.php
+++ application/modules/admin/controllers/MetricsController.php
@@ -7,14 +7,14 @@ class Admin_MetricsController extends Api_AbstractController
 
     public function init()
     {
-        if (!$this->_request->isXmlHttpRequest()) 
+        if (!$this->_request->isXmlHttpRequest())
         {
             $this->_helper->layout->setLayout('admin');
             $this->view->headLink()->appendStylesheet('/css/common.css', 'screen');
             $this->view->headLink()->appendStylesheet('/css/smoothness/jquery-ui-1.8.13.custom.css', 'screen');
             $this->view->headScript()->appendFile('/js/jquery-1.6.1.min.js');
             $this->view->headScript()->appendFile('/js/jquery.form.min.js');
-            $this->view->headScript()->appendFile('/js/jquery-ui-1.8.13.custom.min.js');                        
+            $this->view->headScript()->appendFile('/js/jquery-ui-1.8.13.custom.min.js');
         }
         parent::init();
     }
@@ -27,15 +27,58 @@ class Admin_MetricsController extends Api_AbstractController
     }
 
     public function pollAction()
-    {        
+    {
+        // format input parameters
+
         $lastRowId      = 0;
-        $lastFromPost   = intval($this->_getParam('lastRowId'));
+        $rows           = array();
+        $lastFromPost   = $this->_getParam('lastRowId');
         if($lastFromPost > 0)
         {
             $lastRowId = $lastFromPost;
         }
-        $content = array('lastRowId' => $lastRowId, 'lastUpdated' => date('h:i:s a'));
+
+        $rows = array(
+            array(
+                'userId'        => '1',
+                'userName'      => 'Some User Name',
+                'metricsType'   => 'Page View',
+                'starbar'       => 'Starbar',
+                'dateTime'      => '2011-10-10 10:10:10',
+                'data'          => 'Some data to show',
+            ),
+        );
+
+        // get data
+
+        $this->pollPageViews($rows, $lastRowId);
+        $this->pollSearches($rows, $lastRowId);
+        $this->pollSocialActivity($rows, $lastRowId);
+
+        // send out
+
+        $content = array('lastRowId' => $lastRowId, 'lastUpdated' => date('h:i:s a'), 'rows' => $rows);
         echo json_encode($content);
         exit(0);
+    }
+
+    private function pollPageViews(&$result, &$lastRowId)
+    {
+
+    }
+
+    private function pollSearches(&$result, &$lastRowId)
+    {
+
+    }
+
+    private function pollSocialActivity(&$result, &$lastRowId)
+    {
+
+    }
+
+    private function formatPollResult(&$result, $data)
+    {
+
     }
 }
--- public/modules/admin/metrics/index.js
+++ public/modules/admin/metrics/index.js
@@ -3,21 +3,69 @@
  * @see Timeouts: http://benalman.com/projects/jquery-dotimeout-plugin/
  */
 
-function doPoll(o)
-{    
+function prependRows()
+{
+    if(window._adminPoller.rows.length > 0)
+    {
+        var rows = window._adminPoller.rows;
+        window._adminPoller.rows = [];
+        $.each(rows, function(i, v){
+
+        });
+    }
+}
+
+function doPoll()
+{
     var data = {lastRowId : window._adminPoller.lastRowId};
 
     $.ajax({
-        url:      '/admin/metrics/poll',
-        dataType: 'json',
-        data:     data,
-        success:  function(data)
+        url         : '/admin/metrics/poll',
+        dataType    : 'json',
+        data        : data,
+        success     : function(data)
         {
+            window._adminPoller.lastRowId = data.lastRowId;
             //console.debug(data);
             $('#last-updated').html('Last updated: ' + (data.lastUpdated != undefined ? data.lastUpdated : ''));
+            if(data.rows.length > 0)
+            {
+                window._adminPoller.rows = $.merge(window._adminPoller.rows, data.rows);
+            }
+            //console.debug(window._adminPoller.rows);
+            if(window._adminPoller.rows.length > 0)
+            {
+                if(window._adminPoller.isInit)
+                {
+                    // we do it only oince at page load
+                    prependRows();
+                    window._adminPoller.isInit = false;
+                }
+                else
+                {
+                    // twitter-style updater:
+                    // add clickable div if not exists or update count otherwise
+                    if($('#update-marker').length == 0)
+                    {
+                        $('#updates').prepend('<div id="update-marker">'
+                            +'<a href="javascript:void(null)" class="new-data-available">'
+                                +'New data rows (<span id="new-data-available-count">'
+                                    + window._adminPoller.rows.length + '</span>)</a></div>');
+                    }
+                    else
+                    {
+                        $('#new-data-available-count').html(window._adminPoller.rows.length);
+                    }
+                    $('.new-data-available').unbind('click').bind('click', function()
+                    {
+                        prependRows();
+                        $('#update-marker').remove();
+                    });
+                }
+                
+            }
         }
     });
-
 }
 
 function bindPoll()
@@ -35,7 +83,7 @@ function bindPoll()
 
 function bindAll()
 {
-    window._adminPoller = {lastRowId : 0};
+    window._adminPoller = {lastRowId : 0, isInit : true,  rows : []};
 
     // disallow dummy submits
     $('#dummy').submit(function(){return false;});

3c2728171bfe4380f1206bf724d58629790b6c7a
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-03 14:05:28 +0000

    #191 Admin module access (auth) - 2. Create Admin_CommonController

---
+++ application/models/AdminRole.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Class for handling administrative users
+ * @author alecksmart
+ */
+class AdminUser extends Record
+{
+
+    protected $_tableName = 'user';
+
+
+
+}
--- application/models/AdminUser.php
+++ application/models/AdminUser.php
@@ -1,21 +1,11 @@
 <?php
 /**
- * Class for handling admiistrative users
+ * Class for handling administrative user roles
  * @author alecksmart
  */
 class AdminUser extends Record
 {
 
-    protected $_tableName = 'user';
-
-    public function setPassword($password)
-    {
-        $this->_password = md5($password);
-    }
-
-    public static function getByEmail($email)
-    {
-        return null;
-    }
+    protected $_tableName = 'admin_role';
 
 }

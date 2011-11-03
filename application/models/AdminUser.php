bec08b3065423cc8b021f1eea5b1120baf2b1704
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-03 14:27:23 +0000

    #191 Admin module access (auth) - 2. Create Admin_CommonController

--- application/models/AdminUser.php
+++ application/models/AdminUser.php
@@ -1,11 +1,50 @@
 <?php
 /**
  * Class for handling administrative user roles
+ *
  * @author alecksmart
  */
 class AdminUser extends Record
 {
 
     protected $_tableName = 'admin_user';
+
+    /**
+     * Prepare for writing to database 
+     * 
+     * @param string $password 
+     */
+    public function setPassword($password)
+    {
+        $this->password = md5($password);
+    }
+
+    /**
+     * Get instance according to unique email address
+     * @param string $identity email address
+     */
+    public static function getByEmail($email)
+    {
+        return null;
+    }
+
+    /**
+     * Perform login
+     *
+     * @param type $email
+     * @param type $password
+     */
+    public static function login($email, $password)
+    {
+        
+    }
+
+    /**
+     * Perform logout
+     */
+    public static function logout()
+    {
+        
+    }
 
 }
---
+++ application/models/Form/AdminUser/Login.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Administrative user login form handling
+ *
+ * @author alecksmart
+ */
+class Form_AdminUser_Login extends Zend_Form
+{
+}

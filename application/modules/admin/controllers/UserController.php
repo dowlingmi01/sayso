fad06ad50e41321487be90803f4dc54c512c0f4c
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-04 15:54:47 +0000

    #191 Admin module access (auth) - create login/logout partial

--- application/modules/admin/controllers/UserController.php
+++ application/modules/admin/controllers/UserController.php
@@ -14,8 +14,46 @@ class Admin_UserController extends Admin_CommonController
 
     public function loginAction()
     {
+        if(!$this->_request->isXmlHttpRequest() || $this->auth->hasIdentity())
+        {
+            die('You are not supposed to be here...');
+        }
+
         $this->_helper->layout->setLayout('empty');
         $form = new Form_AdminUser_Login();
+        // String used for javascript
+        $this->view->loginResult = array('ok' => false, 'message' => array());
+
+        if ($this->_request->isPost() && $form->isValid($_POST))
+		{
+            try
+            {
+                $emailAddress = $form->getValue('txtLogin');
+                $password = $form->getValue('passwPassword');
+
+                $adapter = new Zend_Auth_Adapter_DbTable(null, 'admin_user', 'email', 'password', 'MD5(?)');
+                $adapter->setIdentity($emailAddress)->setCredential($password);
+                $result = $this->auth->authenticate($adapter);
+
+                if($result->isValid())
+                {
+                    $this->view->loginResult['ok'] = true;
+                }
+                else
+                {
+                    $this->view->loginResult['message'][] = 'Login failed';
+                }
+            }
+            catch(Exception $e)
+            {
+                $this->view->loginResult['message'][]   = 'Login failed with exception';
+                if(getenv('APPLICATION_ENV') != 'production')
+                {
+                    $this->view->loginResult['message'][] = $e->getMessage();
+                }
+            }
+        }
+
         $this->view->form = $form;
     }
 }
--- public/modules/common.js
+++ public/modules/common.js
@@ -2,6 +2,45 @@
  * @author alecksmart
  */
 
+
+/**
+ * Login section
+ */
+function bindLoginForm()
+{
+    $('#user-login').unbind('submit').bind('submit', function()
+    {
+        window._lastLoginSuccess_ = false;
+        var data = $('#user-login').serialize();
+
+        $.ajax({
+            url         : '/admin/user/login',
+            data        : data,
+            type        : 'POST',
+            success     : function(data)
+            {
+                $('#login-dialog').html(data);
+                if(loginResult != undefined && loginResult.ok)
+                {
+                    // suport for other functions
+                    window._lastLoginSuccess_ = true;
+                    //$('#login').html('Logout').attr('rel', '1');
+                    //bindLogout();
+                    $('#login-dialog').dialog("close");
+                    self.location.reload();
+                    return;
+                }
+                if(loginResult.message != undefined
+                    && loginResult.message.length > 0)
+                {
+                    alert(loginResult.message.join('\n'));
+                }
+                bindLoginForm();
+            }
+        });
+    });
+}
+
 function loadLoginDialog()
 {
     $.ajax({
@@ -9,7 +48,16 @@ function loadLoginDialog()
         success     : function(data)
         {
             $('#login-dialog').html(data);
+            bindLoginForm();
         }
+    });
+}
+
+function bindLogout()
+{
+    $('#login').unbind().bind('click', function()
+    {
+        alert('Logging out not implemented yet!');
     });
 }
 
@@ -19,7 +67,7 @@ function bindDefaults()
     {
         if($(this).attr('rel') > '0')
         {
-            alert('Logging out not implemented yet!')
+            bindLogout();
         }
         else
         {
@@ -31,7 +79,15 @@ function bindDefaults()
     });
 }
 
+/**
+ * Login section end
+ */
+
 $(function()
 {
+    /**
+     * Login section
+     */
+    window._lastLoginSuccess_ = false;
     bindDefaults();
 });

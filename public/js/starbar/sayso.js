bf54858b0daa108342eb18d7f7c13d8d73d72636
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-07 15:21:00 +0000

    #170 Added polling for google live search field changes

--- public/js/starbar/sayso.js
+++ public/js/starbar/sayso.js
@@ -1,13 +1,13 @@
 
-$SQ(function () { 
-    
+$SQ(function () {
+
     // setup
-    
-    var sayso = window.sayso, 
+
+    var sayso = window.sayso,
         starbar = window.sayso.starbar,
         log = window.sayso.log,
         warn = window.sayso.warn;
-    
+
     var ajax = function (options) {
         options.data = $SQ.extend(options.data || {}, {
             auth_key : starbar.authKey,
@@ -19,20 +19,20 @@ $SQ(function () {
         options.dataType = 'jsonp';
         return $SQ.ajax(options);
     };
-    
+
     /**
      * Helper function which can be used outside this context (e.g. in KRL)
-     * 
+     *
      * @example window.sayso.helper.socialActivity('foo.com', 'blah blah', 1)
      */
     window.sayso.helper = new function () {
-        
+
         var _instance = this;
-        
-        // social activity 
-        
+
+        // social activity
+
         // see KRL for Facebook Like logic
-        
+
         this.socialActivity = function (url, content, type_id) {
             ajax({
                 url : 'http://' + sayso.baseDomain + '/api/metrics/social-activity-submit',
@@ -47,14 +47,14 @@ $SQ(function () {
             });
         };
     };
-    
+
     // behavioral tracking
-    
+
     // NOTE: these are currently *always* firing. In the future these should fire only
     // if the current study is set to include them. @todo add conditional logic for that
-    
+
     // page view
-    
+
     ajax({
         url : 'http://' + sayso.baseDomain + '/api/metrics/page-view-submit',
         data : {
@@ -63,25 +63,25 @@ $SQ(function () {
         success : function (response) {
             log('Track page view', response);
         }
-    });    
+    });
 
     // search
-        
+
     var searchType = 0,
         searchRegex = '';
 
     var googleEngineRegexp = /google(\..{2,3})+(\..{2,3})?\//;
-    
+
     if (location.href.match('bing.com/search'))
     {
         searchType = 1; // bing (these ids match lookup_search_engines table)
         searchRegex = /q=([^&]+)&/g;
-    } 
+    }
     else if (googleEngineRegexp.test(location.href))
     {
         searchType = 2; // google
         searchRegex = /&q=([^&]+)&?.*$/g;
-    } 
+    }
     else if (location.href.match('search.yahoo.com'))
     {
         searchType = 3; // yahoo
@@ -90,33 +90,92 @@ $SQ(function () {
 
     //sayso.log('debug >>> ' + location.href);
     //sayso.log('----- >>> ' + location.href.match('search.yahoo.com'));
-    
-    if (searchType) {
+
+    function onSearchEven(url, data)
+    {
+        ajax({
+            url     : url,
+            data    : data,
+            success : function (response) {
+                log('Search', response);
+            }
+        });
+    }
+
+    if (searchType)
+    {
+
         var searchQueryArray = searchRegex.exec(location.href);
         if (searchQueryArray != null && searchQueryArray.length > 1) {
 
-            //sayso.log(searchQueryArray);
+            var searchQuery = searchQueryArray[1];
+            var url = 'http://' + sayso.baseDomain + '/api/metrics/search-engine-submit';
+            var data = {
+                type_id : searchType,
+                query   : searchQuery
+            };
 
-            var searchQuery = searchQueryArray[1]; 
-            ajax({
-                url : 'http://' + sayso.baseDomain + '/api/metrics/search-engine-submit',
-                data : {
-                    type_id : searchType,
-                    query : searchQuery
-                },
-                success : function (response) {
-                    log('Search', response);
-                }
-            });
-        } else {
+            onSearchEven(url, data);
+
+            // We are in google, let's monitor the query field
+            if(searchType == 2)
+            {
+                // remeber initial value in search field
+                var lastQueryValue  = $SQ('input[name=q]').val();
+                // the above is changed and remains so validInterval ms
+                var validInterval   = 2000;
+                // start counting time at 0
+                var startInterval   = 0;
+                // poll each checkInterval ms
+                var checkInterval   = 100;
+                // did we send stats before?
+                // yes, we sent them at the page load already...
+                var statsSent       = true;
+                // do poll
+                $SQ.doTimeout(checkInterval, function()
+                {
+                    // any changes
+                    var currentQueryValue = $SQ('input[name=q]').val();
+                    // yes, reset all
+                    if(currentQueryValue != lastQueryValue)
+                    {
+                        lastQueryValue  = currentQueryValue;
+                        startInterval   = 0;
+                        statsSent       = false;
+                    }
+                    else
+                    {
+                        // increment the start and check
+                        startInterval += checkInterval;
+                        if(startInterval >= validInterval)
+                        {
+                            // no stats send? send it!
+                            if(!statsSent)
+                            {
+                                // send now asynchronously
+                                data['query'] = currentQueryValue;
+                                onSearchEven(url, data);
+                                // but set check synchronously to avoid repeating...
+                                statsSent = true;
+                            }
+                            // reset
+                            startInterval = 0;
+                        }
+                    }
+                    return true;
+                });
+            }
+        } 
+        else 
+        {
             warn('On search page, but no query found');
         }
     }
-    
+
     // Tweets
-    
+
     if (location.hostname.match('twitter.com')) {
-        
+
         var tweet = '';
         $SQ('div.tweet-box textarea').keyup(function () {
             // since there is a race condition between
@@ -131,21 +190,21 @@ $SQ(function () {
             tweet = '';
         });
     }
-    
+
     // Facebook Like
-    
+
     var likeButtons = $SQ('iframe[src*="facebook.com/plugins/like.php"],iframe[src*="facebook.com/widgets/like.php"]');
-    
+
     if (likeButtons.length) {
         var liked = false;
         likeButtons.bind('mouseover', function (eventOver) {
             //sayso.log('Over Like');
-            
+
             // only register 1 Like event per page
             if (liked) return;
-            
+
             var timerRunning = true;
-            
+
             // register Like if mouse stays over for enough time
             // to aim, click and get feedback (button changes)
             var mouseOutTimer = setTimeout(function () {
@@ -154,7 +213,7 @@ $SQ(function () {
                 //sayso.log('Like!');
                 sayso.helper.socialActivity(location.href, '', 1);
             }, 700); // aim + click + feedback
-            
+
             // cancel Like if mouse passes back out quickly
             $SQ(this).unbind('mouseout').bind('mouseout', function (eventOut) {
                 if (timerRunning) {
@@ -164,32 +223,6 @@ $SQ(function () {
             });
         });
     }
-
-    /*$SQ('input[name=q]').bind('change', function(){
-
-        if($SQ(this).saysoBound == undefined)
-        {
-            $SQ(this).saysoBound = true;
-        }
-        else
-        {
-            return;
-        }
-    });
-
-
-    $SQ(document).ajaxComplete(function() {
-        alert("AJAX call completed...");
-    });
-     
-     
-    function onSearchEven(terms)
-    {
-
-    } 
-     
-     */
-
 
 
 });

da92886a270be03d15e6b71927ceeeec73d3b3ae
hamzakubba <hamza@say.so>
2011-11-07 18:35:22 +0000

    Quick fix for previous update

--- scripts/sql/2011-11-07-02-notification_data_changes.sql
+++ scripts/sql/2011-11-07-02-notification_data_changes.sql
@@ -1,1 +1,1 @@
-UPDATE notification_message SET popbox_to_open = 'user-level' WHERE popbox_to_open LIKE 'user-levels');
+UPDATE notification_message SET popbox_to_open = 'user-level' WHERE popbox_to_open LIKE 'user-levels';

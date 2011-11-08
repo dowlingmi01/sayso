249704672f86b17deb2ca74452ba3fd3109586ad
hamzakubba <hamza@say.so>
2011-11-08 16:54:45 +0000

    Added check for IE version to disable jquery effects if using IE version below 9

--- public/js/starbar/starbar-loader.js
+++ public/js/starbar/starbar-loader.js
@@ -293,6 +293,20 @@
                 params.client_user_logged_in = (sayso.client.userLoggedIn ? 'true' : '');
             }
             
+			function getInternetExplorerVersion() {
+				var rv = -1; // Return value assumes failure.
+				if (navigator.appName == 'Microsoft Internet Explorer') {
+					var ua = navigator.userAgent;
+					var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+					if (re.exec(ua) != null)
+						rv = parseFloat(RegExp.$1);
+				}
+				return rv;
+			}
+			
+			var ieVersion = getInternetExplorerVersion();
+			if (ieVersion > -1 && ieVersion < 9) $SQ.fx.off = true;
+			
             $SQ.ajax({
                 dataType: 'jsonp',
                 data : params,

bd7efa0d4f1d6064d371690467ec4023155cea08
davidbjames <davidbjames@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-09 16:14:06 +0000

    Minor fix to SQL FK changed script

--- scripts/sql/2011-11-09-02-update_foreign_keys_admin.sql
+++ scripts/sql/2011-11-09-02-update_foreign_keys_admin.sql
@@ -1,7 +1,7 @@
 SET foreign_key_checks = 0;
 
-ALTER TABLE study_cell_assignment MODIFY user_id int(10) DEFAULT NULL;
 ALTER TABLE study_cell_assignment DROP FOREIGN KEY study_cell_assignment_ibfk_1 ;
+ALTER TABLE study_cell_assignment MODIFY user_id int(10) DEFAULT NULL;
 ALTER TABLE study_cell_assignment ADD FOREIGN KEY study_cell_assignment_user_id ( user_id ) REFERENCES `user` (id) ON DELETE CASCADE ON UPDATE CASCADE ;
 
 SET foreign_key_checks = 1;

3228e3551082437b0075cef0bd3b552c7e69d7de
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-15 18:30:47 +0000

    #257 - fixes

--- scripts/sql/2011-11-15-01-metrics_unified.sql
+++ scripts/sql/2011-11-15-01-metrics_unified.sql
@@ -2,27 +2,19 @@
 -- Table structure for table metrics
 --
 
-DROP TABLE IF EXISTS metrics;
+DROP TABLE IF EXISTS metrics_log;
 
-CREATE TABLE metrics (
+CREATE TABLE metrics_log (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   legacy_id int(10) NOT NULL,
   created datetime NOT NULL,
   user_id int(10) unsigned NOT NULL,
   metrics_type tinyint(3) unsigned NOT NULL,
-  starbar_id int(10) unsigned NOT NULL,
-  param_1 int(11) unsigned NOT NULL DEFAULT '0',
-  param_2 int(11) unsigned NOT NULL DEFAULT '0',
-  param_3 varchar(80) NOT NULL DEFAULT '',
-  param_4 varchar(80) NOT NULL DEFAULT '',
+  starbar_id int(10) unsigned NOT NULL,  
   content text NOT NULL,
   PRIMARY KEY (id),
   KEY created (created),
   KEY user_id (user_id),
   KEY metrics_type (metrics_type),
-  KEY starbar_id (starbar_id),
-  KEY param_1 (param_1),
-  KEY param_2 (param_2),
-  KEY param_3 (param_3(4)),
-  KEY param_4 (param_4(4))
+  KEY starbar_id (starbar_id)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- scripts/sql/2011-11-15-02-triggers.sql
+++ scripts/sql/2011-11-15-02-triggers.sql
@@ -1,14 +1,14 @@
-TRUNCATE TABLE `metrics`;
+TRUNCATE TABLE `metrics_log`;
 
 --
 -- Triggers `metrics_search`
 --
 
-DROP TRIGGER IF EXISTS `metrics_search_to_metrics`;
+DROP TRIGGER IF EXISTS `metrics_search_to_metrics_log`;
 DELIMITER //
-CREATE TRIGGER `metrics_search_to_metrics` AFTER INSERT ON `metrics_search`
+CREATE TRIGGER `metrics_search_to_metrics_log` AFTER INSERT ON `metrics_search`
  FOR EACH ROW BEGIN
-    INSERT INTO metrics
+    INSERT INTO metrics_log
         (legacy_id, created, user_id, metrics_type, starbar_id, content)
         (SELECT
             ms.id, ms.created, ms.user_id, 1, ms.starbar_id, concat(lsa.label, ', query: ', ms.query)
@@ -19,7 +19,28 @@ CREATE TRIGGER `metrics_search_to_metrics` AFTER INSERT ON `metrics_search`
             AND ms.user_id = u.id
             AND ms.starbar_id = s.id
             AND ms.search_engine_id = lsa.id);
-  END
+  END;
 //
 DELIMITER ;
 
+--
+-- Triggers `metrics_page_view`
+--
+
+DROP TRIGGER IF EXISTS `metrics_page_view_to_metrics_log`;
+DELIMITER //
+CREATE TRIGGER `metrics_page_view_to_metrics_log` AFTER INSERT ON `metrics_page_view`
+ FOR EACH ROW BEGIN
+    INSERT INTO metrics_log
+        (legacy_id, created, user_id, metrics_type, starbar_id, content)
+        (SELECT
+            mpv.id, mpv.created, mpv.user_id, 2, mpv.starbar_id, mpv.url
+        FROM
+            metrics_page_view mpv, `user` u, starbar s
+        WHERE
+            mpv.id = NEW.id
+            AND mpv.user_id = u.id
+            AND mpv.starbar_id = s.id);
+  END;
+//
+DELIMITER ;

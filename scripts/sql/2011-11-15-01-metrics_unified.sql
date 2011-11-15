a540553a40fabd25316372a598d0328218cd9235
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-15 17:38:11 +0000

    #257 - fast table

---
+++ scripts/sql/2011-11-15-01-metrics_unified.sql
@@ -0,0 +1,26 @@
+--
+-- Table structure for table metrics
+--
+
+DROP TABLE IF EXISTS metrics;
+CREATE TABLE metrics (
+  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  created datetime NOT NULL,
+  user_id int(10) unsigned NOT NULL,
+  metrics_type tinyint(3) unsigned NOT NULL,
+  starbar_id int(10) unsigned NOT NULL,
+  param_1 int(11) unsigned NOT NULL DEFAULT '0',
+  param_2 int(11) unsigned NOT NULL DEFAULT '0',
+  param_3 varchar(80) NOT NULL DEFAULT '',
+  param_4 varchar(80) NOT NULL DEFAULT '',
+  content text NOT NULL,
+  PRIMARY KEY (id),
+  KEY created (created),
+  KEY user_id (user_id),
+  KEY metrics_type (metrics_type),
+  KEY starbar_id (starbar_id),
+  KEY param_1 (param_1),
+  KEY param_2 (param_2),
+  KEY param_3 (param_3(4)),
+  KEY param_4 (param_4(4))
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;

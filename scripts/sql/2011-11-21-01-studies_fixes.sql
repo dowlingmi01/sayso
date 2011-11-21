2af0c6413f551d2cc580135c5cd81d10b9727dee
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-21 15:29:27 +0000

    #263 - tab 1 js validation

--- scripts/sql/2011-11-21-01-studies_fixes.sql
+++ scripts/sql/2011-11-21-01-studies_fixes.sql
@@ -1,6 +1,12 @@
 SET foreign_key_checks = 0;
 
-ALTER TABLE `study` ADD `study_id` VARCHAR( 16 ) NOT NULL AFTER `name` ;
-ALTER TABLE `study` ADD `study_type` TINYINT UNSIGNED NOT NULL DEFAULT '1' AFTER `id`;
+ALTER TABLE study ADD study_id VARCHAR( 16 ) NOT NULL AFTER name ;
+ALTER TABLE study ADD study_type TINYINT UNSIGNED NOT NULL DEFAULT '1' AFTER id;
+
+UPDATE sayso.lookup_social_activity_type SET label = 'Facebook Like',
+description = 'Facebook Like button clicks' WHERE lookup_social_activity_type.id =1;
+
+UPDATE sayso.lookup_social_activity_type SET label = 'Twitter',
+description = 'Twitter tweets' WHERE lookup_social_activity_type.id =2;
 
 SET foreign_key_checks = 1;

f8b11aaf7a6e01fb3ba9a7db462e001c334c1179
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-21 17:42:26 +0000

    #263 - html helper

---
+++ application/models/Form/Markup/Element/AnyHtml.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Add any Html to Zend Form
+ *
+ * @author alecksmart
+ */
+
+class Form_Markup_Element_AnyHtml extends Zend_Form_Element_Xhtml
+{
+    public $helper = 'formNote';
+}

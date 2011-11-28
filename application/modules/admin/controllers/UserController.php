f295829559576e85c8385685d5c5fcf778536e34
alecksmart <alecksmart@9acc3c26-c38b-476f-80b9-92511db9b07f>
2011-11-28 12:37:33 +0000

    #191 SAYSO - users admin CRUD: list users

--- application/modules/admin/controllers/UserController.php
+++ application/modules/admin/controllers/UserController.php
@@ -24,8 +24,76 @@ class Admin_UserController extends Admin_CommonController
             $this->_helper->viewRenderer->setNoRender(true);
         }
 
-        $this->view->headScript()->appendFile('/modules/admin/user/index.js');        
+        $this->view->headScript()->appendFile('/modules/admin/user/index.js');
         $this->view->addLink = '<a href="' . $this->view->url(array('action' => 'add')) . '">Add New</a>';
+
+        $grid   = new Data_Markup_Grid();
+        $select = Zend_Registry::get('db')->select()->from('admin_user');
+        $grid->setSource(new Bvb_Grid_Source_Zend_Select($select));
+        $grid->setGridColumns(array('id', 'email', 'first_name', 'last_name', 'created', 'modified', 'edit', 'delete'));
+
+        $extraColumnEdit = new Bvb_Grid_Extra_Column();
+		$extraColumnEdit
+			->position('right')
+			->name('edit')
+			->title(' ')
+			->callback(
+                array(
+                    'function'  => array($this, 'generateEditButtonLink'),
+                    'params'    => array('{{id}}')
+                )
+            );
+        $grid->addExtraColumns($extraColumnEdit);
+
+        $extraColumnDelete = new Bvb_Grid_Extra_Column();
+		$extraColumnDelete
+			->position('right')
+			->name('delete')
+			->title(' ')
+			->callback(
+                array(
+                    'function'  => array($this, 'generateDeleteButtonLink'),
+                    'params'    => array('{{id}}')
+                )
+            );
+        $grid->addExtraColumns($extraColumnDelete);
+
+        $grid->updateColumn('id',
+			array(
+                'class' => 'align-right'
+			)
+		);
+        $grid->updateColumn('email',
+			array(
+				'callback' => array(
+					'function'  => array($this, 'generateEditLink'),
+					'params'    => array('{{id}}', '{{email}}')
+				),
+                'class' => 'align-left important'
+			)
+		);
+
+        $this->view->grid = $grid->deploy();
+    }
+
+    public function generateEditLink($id, $email)
+    {
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL) ? $email : '';
+        
+        return '<a href="' . $this->view->url(array('action' => 'edit', 'study_id' => intval($id))) . '">'.
+            ($email ? $email : '<span class="disabled">email malformed</span>') .'</a>';
+    }
+
+    public function generateEditButtonLink($id)
+    {
+        return  '<a href="' . $this->view->url(array('action' => 'edit', 'study_id' => intval($id)))
+                    . '" class="button-edit" title="Edit"></a>';
+    }
+
+    public function generateDeleteButtonLink($id)
+    {
+        return  '<a href="' . $this->view->url(array('action' => 'delete', 'study_id' => intval($id)))
+                    . '" class="button-delete" title="Delete"></a>';
     }
 
     public function loginAction()
--- public/modules/admin/user/index.js
+++ public/modules/admin/user/index.js
@@ -4,10 +4,14 @@
 
 function bindLocal()
 {
-    /*$('#list').addClass('admin-table');
+    $('#list').addClass('admin-table');
 
-    // delete action needs confirmation...
-    $('.button-delete').click(function()
+    $('#list a').click(function(){
+        alert('Not implemented yet!');
+        return false;
+    });
+
+    /*$('.button-delete').click(function()
     {
         return confirm('Delete this entry?') ? true : false;
     });*/

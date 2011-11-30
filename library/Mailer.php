939e7b524bae2f725113b91bc350f19cd519098f
hamzakubba <hamza@say.so>
2011-11-30 21:27:16 +0000

    Quick fix with previous update

--- library/Mailer.php
+++ library/Mailer.php
@@ -5,7 +5,7 @@ class Mailer extends Zend_Mail
 {
     public function setBodyMultilineText($txt, $charset = null, $encoding = Zend_Mime::ENCODING_QUOTEDPRINTABLE)
     {
-    	$txt = preg_replace('\s\s+', ' ', $txt);
+    	$txt = str_replace('					', '', $txt);
     	parent::setBodyText($txt, $charset, $encoding);
 	}
 }

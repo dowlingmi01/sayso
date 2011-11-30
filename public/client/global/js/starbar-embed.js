c572035b9b5b057c64952e10bb5df8e3415b3f61
hamzakubba <hamza@say.so>
2011-11-30 22:14:35 +0000

    Fix for previous fix

--- public/client/global/js/starbar-embed.js
+++ public/client/global/js/starbar-embed.js
@@ -151,8 +151,6 @@
 	                        container.fadeTo('slow', 1, function () {
 	                            if (!loginCookie || !userUniqueId) {
                             		alert('Please log in first to install the app');
-						            // delete the install cookie
-						            setCookie('sayso-install', null, -10);
 	                                $SQ('#sayso-onboard,#sso_wrapper,#sayso-container').hide();
 	                                // fire login callback 
 	                                if (sayso.client.loginCallback) sayso.client.loginCallback();

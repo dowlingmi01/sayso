c060f727627659838d97c1ddbaba43ba811f08bb
hamzakubba <hamza@say.so>
2011-12-08 23:06:05 +0000

    3 new polls

---
+++ scripts/sql/2011-12-08-01-new_polls.sql
@@ -0,0 +1,6 @@
+INSERT INTO survey (ordinal, user_id, type, origin, starbar_id, title, created, external_id, external_key, premium, number_of_answers)
+VALUES (340, 1, 'poll', 'SurveyGizmo', 1, 'Would you ever pay to take private guitar lessons online?', now(), '746361', '5TZNLTFUVR1IY0PAU0FC2B41D54UH4', null, 4);
+INSERT INTO survey (ordinal, user_id, type, origin, starbar_id, title, created, external_id, external_key, premium, number_of_answers)
+VALUES (350, 1, 'poll', 'SurveyGizmo', 1, 'How long did you play before you felt like you found "your sound"?', now(), '746399', '7U5CWVV2FY4SQEG479T8BZTHP522AP', null, 4);
+INSERT INTO survey (ordinal, user_id, type, origin, starbar_id, title, created, external_id, external_key, premium, number_of_answers)
+VALUES (360, 1, 'poll', 'SurveyGizmo', 1, 'Old rockers – hang it up or keep on rocking? ', now(), '746505', 'Z5TPP9IEWD6AP119OOMU90U9QU77GV', null, 4);

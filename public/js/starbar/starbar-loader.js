9fad238368829468f6776de63aadf502aba0b484
hamzakubba <hamza@say.so>
2011-12-26 21:03:54 +0000

    Second CrossRider attempt...

--- public/js/starbar/starbar-loader.js
+++ public/js/starbar/starbar-loader.js
@@ -10,7 +10,62 @@
  */
 (function () {
 
-	var sayso = window.sayso,
+	var baseDomain = appAPI.db.get('baseDomain');
+	var environment = appAPI.db.get('environment');
+
+	var starbarId = appAPI.db.get("starbarId") || 0;
+	var userId = appAPI.db.get("userId") || 0;
+	var userKey = appAPI.db.get("userKey") || '';
+	var authKey = appAPI.db.get("authKey") || '';
+	var visibleState = appAPI.db.get("visibleState") || "open";
+	var notificationsState = appAPI.db.get("notificationsState") || 'ready';
+	var profileState = appAPI.db.get("profileState") || 'ready';
+	var gameState = appAPI.db.get("gameState") || 'ready';
+	var windowWidth = appAPI.db.get("windowWidth") || 1000;
+	var windowHeight = appAPI.db.get("windowHeight") || 1000;
+
+	var flags = appAPI.db.get("flags") || 'none';
+	var studies = appAPI.db.get("studies") || '';
+	var studiesTimestamp = appAPI.db.get("studiesTimestamp") || '';
+	var adTargets = appAPI.db.get("adTargets") || '{}';
+
+	$jquery('body').append('<div id="sayso-starbar" style="position: fixed; left: 0px; bottom: 0px; width: 100%; background: none; margin-bottom: -3px; z-index: 9999;"></div>');
+
+	// setup global variables/functions
+
+	if (!window.sayso) window.sayso = {};
+	window.sayso.debug = false;
+	window.sayso.baseDomain = baseDomain;
+	window.sayso.environment = environment;
+	window.sayso.flags = flags;
+	window.sayso.starbar = {
+		id : starbarId,
+		authKey : authKey,
+		user : {
+			id : userId,
+			key : userKey
+		},
+		state : {
+			visibility : visibleState,
+			notifications : notificationsState,
+			profile : profileState,
+			game : gameState
+		},
+		context : {
+			windowWidth : windowWidth,
+			windowHeight : windowHeight
+		},
+		loaded : false
+	};
+	window.sayso.study = {
+		studies : studies,
+		studiesTimestamp : studiesTimestamp,
+		adTargets : adTargets
+
+	};
+
+    // load the Starbar
+    	var sayso = window.sayso,
 		starbarContainer = document.getElementById('sayso-starbar'),
 
 		urlMatchPrepend = '^(?:http|https){1}://(?:[\\w.-]+)?',

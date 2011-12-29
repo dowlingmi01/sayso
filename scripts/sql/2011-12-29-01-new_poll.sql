c25dd47fc1f675c448a77853ccad85841137e21c
hamzakubba <hamza@say.so>
2011-12-29 19:26:29 +0000

    Final new poll (premium too!)

---
+++ scripts/sql/2011-12-29-01-new_poll.sql
@@ -0,0 +1,2 @@
+INSERT INTO survey (ordinal, user_id, type, origin, starbar_id, title, created, external_id, external_key, premium, number_of_answers)
+VALUES (520, 1, 'poll', 'SurveyGizmo', 1, 'What gear would you like to see more of on Hello Music?', now(), '762707', '6LS170JMUKZH4OPJUFTP707D98L14J', 1, 8);

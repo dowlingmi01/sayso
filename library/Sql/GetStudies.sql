e0b53d0dbeeb45196417448e4d00efd81846bedf
hamzakubba <hamza@say.so>
2012-03-02 20:33:09 +0000

    Added new fields to study get all sql

--- library/Sql/GetStudies.sql
+++ library/Sql/GetStudies.sql
@@ -1,12 +1,12 @@
-SELECT 
+SELECT
 	s.id AS study_id, s.user_id AS study_user_id, s.name AS study_name, s.description AS study_description, s.size AS study_size, s.size_minimum AS study_size_minimum, s.begin_date AS study_begin_date, s.end_date AS study_end_date, s.click_track AS study_click_track, s.created AS study_created, s.modified AS study_modified,
 	c.id AS study_cell_id, c.study_id AS study_cell_study_id, c.description AS study_cell_description, c.size AS study_cell_size, c.cell_type AS study_cell_cell_type, c.created AS study_cell_created, c.modified AS study_cell_modified,
-	t.id AS study_tag_id, t.user_id AS study_tag_user_id, t.name AS study_tag_name, t.tag AS study_tag_tag, t.target_url AS study_tag_target_url, t.created AS study_tag_created, t.modified AS study_tag_modified,
+	t.id AS study_tag_id, t.user_id AS study_tag_user_id, t.name AS study_tag_name, t.type AS study_tag_type, t.tag AS study_tag_tag, t.target_url AS study_tag_target_url, t.created AS study_tag_created, t.modified AS study_tag_modified,
 	d.id AS study_domain_id, d.user_id AS study_domain_user_id, d.domain AS study_domain_domain, d.created AS study_domain_created, d.modified AS study_domain_modified,
-	sc.id AS study_creative_id, sc.user_id AS study_creative_user_id, sc.mime_type_id AS study_creative_mime_type_id, sc.name AS study_creative_name, sc.url AS study_creative_url, sc.target_url AS study_creative_target_url, sc.created AS study_creative_created, sc.modified AS study_creative_modified
+	sc.id AS study_creative_id, sc.user_id AS study_creative_user_id, sc.mime_type_id AS study_creative_mime_type_id, sc.name AS study_creative_name, sc.type AS study_creative_type, sc.ad_title AS study_creative_ad_title, sc.ad_description AS study_creative_ad_description, sc.url AS study_creative_url, sc.target_url AS study_creative_target_url, sc.created AS study_creative_created, sc.modified AS study_creative_modified
 FROM
 	study s
-/* add study_search_engines_map and study_social_activity_type_map */	
+/* add study_search_engines_map and study_social_activity_type_map */
 LEFT JOIN
 	study_cell c ON s.id = c.study_id
 LEFT JOIN
@@ -21,4 +21,3 @@ LEFT JOIN
 	study_creative_tag_map ctm ON t.id = ctm.tag_id
 LEFT JOIN
 	study_creative sc ON ctm.creative_id = sc.id
-	

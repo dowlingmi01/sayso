954acd14ba7a3a2dcc010bd06bbcb6c24a99b3c1
pconnolly <pconnolly@say.so>
2012-07-19 18:26:23 +0000

    #778: Wire up 'Ready to get started' text

--- public/client/movie/landing/js/functions.js
+++ public/client/movie/landing/js/functions.js
@@ -5,19 +5,20 @@
 /* trigger when page is ready */
 $(document).ready(function (){
 	var sectionNav = $('#section-nav');
+	var jumpNav = $('#jump');
 	var slideshows = $('.slideshow');
-	
+
 	var termsCheckbox = $('#agreeterms');
-	
+
 	var btnSubmit = $('#btn-submit');
-	
+
 	slideshows.each(function(){
 		var mySlideshow = $(this);
 		var sectionID = mySlideshow.parents('.section').attr('id');
-		
+
 		var myPrev = $('#'+sectionID+' .slide-nav .prev');
 		var myNext = $('#'+sectionID+' .slide-nav .next');
-		
+
 		$('.slides',mySlideshow).cycle({
 			timeout: 0,
 			fx:	'fade',
@@ -25,29 +26,43 @@ $(document).ready(function (){
 			prev:	myPrev,
 			next: myNext
 		});
-		
+
 	});
 
-	
+
 	$('ul li a',sectionNav).click(function (e) {
 	  e.preventDefault();
 	  validateInstall();
 	  $(this).tab('show');
-	  
-	  var myClass = $(this).attr('data-class');	  
+
+	  var myClass = $(this).attr('data-class');
 	  sectionNav.attr('class','');
 	  sectionNav.attr('class',myClass);
-	  
+
 	  //$('#'+myClass+' #slideshow #slides').cycle();
-	  
-	  
+
+
 	});
-	
+
+	$('a',jumpNav).click(function (e) {
+	  e.preventDefault();
+	  validateInstall();
+	  $(this).tab('show');
+
+	  var myClass = $(this).attr('data-class');
+	  sectionNav.attr('class','');
+	  sectionNav.attr('class',myClass);
+
+	  //$('#'+myClass+' #slideshow #slides').cycle();
+
+
+	});
+
 	$('a[data-toggle="tab"]').on('shown', function (e) {
 	  e.target // activated tab
-	  e.relatedTarget // previous tab	  
+	  e.relatedTarget // previous tab
 	})
-	
+
 	function validateInstall() {
 		var msg = false;
 		if( window.$SaySoExtension )
@@ -66,19 +81,19 @@ $(document).ready(function (){
 		}
 	}
 	var emailEl = $('#input-email'), passwordEl = $('#input-password'), confirmationEl = $('#input-confirmation');
-	
+
 	termsCheckbox.on('click', activateGrabIt);
 	emailEl.on('keyup change', activateGrabIt);
 	passwordEl.on('keyup change', activateGrabIt);
 	confirmationEl.on('keyup change', activateGrabIt);
-	
+
 	function activateGrabIt() {
 		if( !validateFields() )
 			btnSubmit.removeAttr('disabled');
 		else
 			btnSubmit.attr('disabled',true);
 	}
-	
+
 	function validateFields() {
 		var emailadd = emailEl.val();
 		if( emailadd.length < 1 )
@@ -97,15 +112,15 @@ $(document).ready(function (){
 			return "Woops - Your passwords do not match.<br>Please reenter your password";
 		if( pwd1.length < 6 || pwd1.length > 12 )
 			return "Woops - Your password needs to have between 6 and 12 characters.<br>Please reenter your password";
-		
+
 		if( !termsCheckbox.is(':checked') )
 			return "Woops - Please accept the terms and conditions"
-				
+
 		return false;
 	}
-	
+
 	btnSubmit.on('click', onGrabIt);
-	
+
 	function onGrabIt(e) {
 		e.preventDefault();
 		var errMsg;
@@ -130,7 +145,7 @@ $(document).ready(function (){
 		} else
 			showWarning(errMsg);
 	}
-	
+
 	function onPasswordResponse( response ) {
 		if (response.status && response.status == "error") {
 			showWarning(response.data.message);
@@ -144,18 +159,18 @@ $(document).ready(function (){
 			location.href = downloadLocation;
 		}
 	}
-	
+
 }); // end document.ready
 
 
 /* optional triggers
 
 $(window).load(function() {
-	
+
 });
 
 $(window).resize(function() {
-	
+
 });
 
 */

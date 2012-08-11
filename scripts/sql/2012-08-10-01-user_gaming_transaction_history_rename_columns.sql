25af5ecbf8708934ebf6653ab3897abcc3d38d33
pconnolly <pconnolly@say.so>
2012-08-11 16:42:17 +0000

    Change column names on view user_gaming_transaction_history

---
+++ scripts/sql/2012-08-10-01-user_gaming_transaction_history_rename_columns.sql
@@ -0,0 +1,3 @@
+ALTER TABLE user_gaming_transaction_history change points experience_points decimal(5,2);
+ALTER TABLE user_gaming_transaction_history change currency redeemable_points decimal(5,2);
+

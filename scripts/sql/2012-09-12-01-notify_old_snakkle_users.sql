7dd75cd861a0fa405870e653b7116899a23d2dea
sdaicz <sdaicz@users.noreply.github.com>
2012-09-12 21:19:13 +0000

    Minor fix to snakkle notification script. Already fixed in prod db.

--- scripts/sql/2012-09-12-01-notify_old_snakkle_users.sql
+++ scripts/sql/2012-09-12-01-notify_old_snakkle_users.sql
@@ -1,23 +1,23 @@
 START TRANSACTION
 ;
 INSERT notification_message
-     ( survey_id, notification_message_group_id, ordinal
+     ( notification_message_group_id, ordinal
      , message
      , notification_area, popbox_to_open, color, created )
 VALUES
-     ( 71, 301, 3
+     ( 301, 3
      , 'Welcome to Movie Say.So! Your say-so snakk-le has become an all-things-movies meal. Check out new content and rewards!'
      , 'alerts', NULL, 'Green', now()
      ),
-     ( 71, 301, 4
+     ( 301, 4
      , 'To claim the bucks and stars you earned in Snakkle Say.So, send an email to info@say.so with subject CINEBUCKS. We\'ll get you sorted!'
      , 'alerts', NULL, 'Green', now()
      ),
-     ( 71, 301, 5
+     ( 301, 5
      , 'Snakkle Say.So has become Movie Say.So. All new content, rewards and giveaways!'
      , 'alerts', NULL, 'Green', now()
      ),
-     ( 71, 301, 6
+     ( 301, 6
      , 'Convert your Snakkle Bucks to CineBucks. Email info@say.so with the subject line "CINEBUCKS"'
      , 'alerts', NULL, 'Green', now()
      )

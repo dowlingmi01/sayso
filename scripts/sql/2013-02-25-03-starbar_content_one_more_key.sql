6b13bd16b4933a025367de6177a12528d2a2a8f9
hamzakubba <hamza@say.so>
2013-02-26 02:58:45 +0000

    Added starbar content (POPUP_USER_LEVEL_ICONS_ON_CAROUSEL) and associated code (to control number of user levels shown on the carousel for each starbar)

---
+++ scripts/sql/2013-02-25-03-starbar_content_one_more_key.sql
@@ -0,0 +1,8 @@
+INSERT INTO starbar_content_key (id, title) VALUES
+(51, 'POPUP_USER_LEVEL_ICONS_ON_CAROUSEL');
+
+INSERT INTO `starbar_content` (`starbar_id`, `starbar_content_key_id`, `content`) VALUES
+(NULL,51,'4'),
+(3,51,'3'),
+(4,51,''),
+(5,51,'4');

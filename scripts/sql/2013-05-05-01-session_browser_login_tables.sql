8b92c62089476b5805bbe3883a5c482eb2a61b1c
rzeszut <support@obscurecloud.com>
2013-06-25 20:59:54 +0000

    fixed so that scripts run in proper order

--- scripts/sql/2013-05-05-01-session_browser_login_tables.sql
+++ scripts/sql/2013-05-05-01-session_browser_login_tables.sql
@@ -1,20 +1,4 @@
-#browsers (unique) 
-DROP TABLE IF EXISTS browser;
-CREATE TABLE `browser` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `browser_type_id` int(11) NOT NULL,
-  `major_version` smallint(6) DEFAULT NULL,
-  `minor_version` smallint(6) DEFAULT NULL,
-  `agent_string` varchar(2000) DEFAULT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `comment` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `browser_type_id_idx` (`browser_type_id`),
-  CONSTRAINT `browser_type_id` FOREIGN KEY (`browser_type_id`) REFERENCES `browser_type` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
-
 #browser types
-DROP TABLE IF EXISTS `browser_type`;
 CREATE TABLE `browser_type` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
@@ -22,26 +6,27 @@ CREATE TABLE `browser_type` (
   UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
+#browsers (unique) 
+ALTER TABLE `browser` CHANGE COLUMN `agent_string` `agent_string` VARCHAR(255) NULL DEFAULT NULL;
+ALTER TABLE `browser` drop column `browser_type`;
+ALTER TABLE `browser` change version minor_version SMALLINT;
+ALTER TABLE `browser` ADD `comment` VARCHAR(45);
+ALTER TABLE `browser` ADD `browser_type_id` INT NOT NULL;
+ALTER TABLE `browser` ADD CONSTRAINT `b_browser_type_id`
+                             FOREIGN KEY (`browser_type_id`)
+                             REFERENCES `browser_type` (`id`);
+ALTER TABLE `browser` ADD INDEX `unique_browser` USING HASH (`agent_string`); 
+
 #user session (not unique, new one started every time a user authenticates, user_session_id sent to client at that time, then user_session_id (or user_key, see below) is sent by the client with every request)
-DROP TABLE IF EXISTS `user_session`;
-CREATE TABLE `user_session` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `browser_id` int(11) NOT NULL,
-  `session_key` varchar(32) NOT NULL,
-  `expired` tinyint(4) DEFAULT '0',
-  `ip` int(10) unsigned DEFAULT NULL,
-  `new_session_id` int(11) DEFAULT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `us_session_key_unique` (`session_key`),
-  KEY `us_user_id` (`user_id`),
-  KEY `us_browser_id` (`browser_id`),
-  CONSTRAINT `us_browser_id` FOREIGN KEY (`browser_id`) REFERENCES `browser` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `us_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+ALTER TABLE `user_session` ADD `session_key` varchar(32) NOT NULL;
+ALTER TABLE `user_session` ADD `expired` varchar(32) NOT NULL;
+ALTER TABLE `user_session` ADD `ip` int(10) unsigned DEFAULT NULL;
+ALTER TABLE `user_session` ADD `new_user_session_id` int(11) DEFAULT NULL;
+ALTER TABLE `user_session` ADD UNIQUE KEY `us_session_key_unique` (`session_key`);
+
 
 #records active bans on ip addresses
+DROP TABLE IF EXISTS `login_ban_ip`;
 CREATE TABLE `login_ban_ip` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` int(10) unsigned DEFAULT NULL,
@@ -51,14 +36,16 @@ CREATE TABLE `login_ban_ip` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 #records failed login attempts. use the code to remove time expired strikes
+DROP TABLE IF EXISTS `login_strikes_ip`;
 CREATE TABLE `login_strikes_ip` (
-  `time` int(11) NOT NULL,
-  `ip` int(10) unsigned DEFAULT NULL
+  `ip` int(10) unsigned NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
 #records failed login attempt strikes against a username. use the code to remove expired strikes
+DROP TABLE IF EXISTS `login_strikes_user`;
 CREATE TABLE `login_strikes_user` (
-  `time` int(11) NOT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`time`)
+  `username` varchar(45) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;

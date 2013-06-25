e788e862821c799df5ce2cf8769f9f8ba28afc70
hamzakubba <hamza@say.so>
2013-06-25 22:33:28 +0000

    Added CONFIG_IMAGE_PATH to starbar content to use for images in markup

---
+++ scripts/sql/2013-06-25-01-starbar_content_key_for_image_path.sql
@@ -0,0 +1,8 @@
+INSERT INTO starbar_content_key (id, title) VALUES
+(52, 'CONFIG_IMAGE_PATH');
+
+INSERT INTO `starbar_content` (`starbar_id`, `starbar_content_key_id`, `content`) VALUES
+(NULL,52,'/browserapp/images/%CONFIG_SHORT_TITLE%/'),
+(3,52,NULL),
+(4,52,NULL),
+(5,52,NULL);

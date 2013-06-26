eb637fc1f9952a454632108e8bcdfd4577f75bcf
sdaicz <sdaicz@users.noreply.github.com>
2013-06-26 17:19:52 +0000

    Iframe communication: add frameId and sending events from top frame to iframe. re #1072

--- public/browserapp/js/src/framecomm.js
+++ public/browserapp/js/src/framecomm.js
@@ -1,4 +1,6 @@
 sayso.module.frameComm = (function(global, $, dommsg, api) {
+	var frames = [];
+	var frameId = 0;
 	function handleApiRequests( data, source ) {
 		api.doRequests(data.requests, function(response) {
 			if( data.id )
@@ -6,9 +8,24 @@ sayso.module.frameComm = (function(global, $, dommsg, api) {
 		});
 	}
 	function handleEvent( data ) {
-		$(global.document).trigger('sayso:iframe-' + data.name, data.data);
+		$(global.document).trigger('sayso:iframe-' + data.name, [{frame_id: data.frame_id, data: data.data}]);
+	}
+	function setURL( $iframe, url, useParam ) {
+		frameId++;
+		frames[frameId] = $iframe[0];
+		var sep = useParam ? (url.indexOf('?') >= 0 ? '&' : '?') : '#';
+		url = url + sep + 'frame_id=' + frameId;
+		$iframe.attr('src', url);
+		return frameId;
+	}
+	function fireEvent( frameId, name, data ) {
+		frames[frameId].contentWindow.postMessage(JSON.stringify(['sayso-iframe-event', {name: name, data: data}]), '*');
 	}
 	dommsg.addHandler('iframe-api-requests', handleApiRequests);
 	dommsg.addHandler('iframe-event', handleEvent);
+	return {
+		setURL: setURL,
+		fireEvent: fireEvent
+	};
 })(this, jQuery, sayso.module.dommsg, sayso.module.api)
 ;
--- public/browserapp/js/src/iframe/framecomm.js
+++ public/browserapp/js/src/iframe/framecomm.js
@@ -1,9 +1,23 @@
-sayso.module.frameComm = (function(global) {
+sayso.module.frameComm = (function(global, dommsg) {
+	var frameId;
+	var paramName = 'frame_id';
+	var listeners = {};
 	function fireEvent( name, data ) {
-		global.top.postMessage( JSON.stringify(['sayso-iframe-event', {name: name, data: data}]), '*');
+		global.top.postMessage( JSON.stringify(['sayso-iframe-event', {name: name, frame_id: frameId, data: data}]), '*');
 	}
+	function listen( name, callback ) {
+		listeners[name] = callback;
+	}
+	function handleEvent( data ) {
+		if( listeners[data.name] )
+			listeners[data.name](data.data);
+	}
+	dommsg.addHandler('iframe-event', handleEvent);
+	if( global.location.hash.slice(0, paramName.length + 2) === '#' + paramName + '=' )
+		frameId = parseInt(global.location.hash.slice(paramName.length + 2), 10);
 	return {
-		fireEvent: fireEvent
+		fireEvent: fireEvent,
+		listen: listen
 	};
-})(this)
+})(this, sayso.module.dommsg)
 ;

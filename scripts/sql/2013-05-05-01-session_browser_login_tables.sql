3619a337610942807843e41277629ef29466b967
rzeszut <support@obscurecloud.com>
2013-07-01 18:52:11 +0000

    db script for user session and browser and login tables.

--- scripts/sql/2013-05-05-01-session_browser_login_tables.sql
+++ scripts/sql/2013-05-05-01-session_browser_login_tables.sql
@@ -4,26 +4,36 @@ CREATE TABLE `browser_type` (
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
-#browsers (unique) 
-ALTER TABLE `browser` CHANGE COLUMN `agent_string` `agent_string` VARCHAR(255) NULL DEFAULT NULL;
-ALTER TABLE `browser` drop column `browser_type`;
-ALTER TABLE `browser` change version minor_version SMALLINT;
-ALTER TABLE `browser` ADD `comment` VARCHAR(45);
-ALTER TABLE `browser` ADD `browser_type_id` INT NOT NULL;
-ALTER TABLE `browser` ADD CONSTRAINT `b_browser_type_id`
-                             FOREIGN KEY (`browser_type_id`)
-                             REFERENCES `browser_type` (`id`);
-ALTER TABLE `browser` ADD INDEX `unique_browser` USING HASH (`agent_string`); 
+#browsers (unique)
+CREATE TABLE `browser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `browser_type_id` int(11) NOT NULL,
+  `major_version` smallint(6) DEFAULT NULL,
+  `minor_version` smallint(6) DEFAULT NULL,
+  `agent_string` varchar(255) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_browser` (`agent_string`) USING HASH,
+  CONSTRAINT `b_browser_type_id` FOREIGN KEY (`browser_type_id`) REFERENCES `browser_type` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
-#user session (not unique, new one started every time a user authenticates, user_session_id sent to client at that time, then user_session_id (or user_key, see below) is sent by the client with every request)
-ALTER TABLE `user_session` ADD `session_key` varchar(32) NOT NULL;
-ALTER TABLE `user_session` ADD `expired` varchar(32) NOT NULL;
-ALTER TABLE `user_session` ADD `ip` int(10) unsigned DEFAULT NULL;
-ALTER TABLE `user_session` ADD `new_user_session_id` int(11) DEFAULT NULL;
-ALTER TABLE `user_session` ADD UNIQUE KEY `us_session_key_unique` (`session_key`);
-
+#user_session
+CREATE TABLE `user_session` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `browser_id` int(11) NOT NULL,
+  `session_key` varchar(32) NOT NULL,
+  `expired` datetime DEFAULT NULL,
+  `ip` int(10) unsigned DEFAULT NULL,
+  `new_user_session_id` int(11) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `us_session_key_unique` (`session_key`),
+  CONSTRAINT `b_browser_id` FOREIGN KEY (`browser_id`) REFERENCES `browser` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 #records active bans on ip addresses
 DROP TABLE IF EXISTS `login_ban_ip`;
@@ -41,7 +51,6 @@ CREATE TABLE `login_strikes_ip` (
   `ip` int(10) unsigned NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 #records failed login attempt strikes against a username. use the code to remove expired strikes
 DROP TABLE IF EXISTS `login_strikes_user`;

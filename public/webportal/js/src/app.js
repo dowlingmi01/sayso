35a7b77e9ad0ef911044bfd31ec181f68556129e
csteelesayso <cwsteele13@gmail.com>
2013-07-03 23:48:03 +0000

    Use typeof undefined.

--- public/webportal/js/src/app.js
+++ public/webportal/js/src/app.js
@@ -569,7 +569,7 @@ sayso.module.webportal = (function(global, $, state, api, Handlebars, comm) {
                             starbar_id : starbarId,
                             email: emailAddress
                         }, function(response){
-                            if (typeof(response.responses['default'].variables.success) !== undefined) {
+                            if (typeof response.responses['default'] !== typeof undefined  && typeof response.responses['default'].variables  !== typeof undefined) {
                                 var success = response.responses['default'].variables.success;
 
                                 if (success === true){
@@ -658,7 +658,7 @@ sayso.module.webportal = (function(global, $, state, api, Handlebars, comm) {
                         verification_code : resetCode,
                         new_password: newPassword
                     }, function(response){
-                        if (typeof(response.responses['default'].variables.success) !== undefined) {
+                        if (typeof response.responses['default'] !== typeof undefined  && typeof response.responses['default'].variables  !== typeof undefined) {
                             var success = response.responses['default'].variables.success;
 
                             if (success === true){

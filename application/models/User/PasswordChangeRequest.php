23220bffe6bd39e2f09db52bf5e9aef2e25f54cc
hamzakubba <hamza@say.so>
2013-07-03 22:47:44 +0000

    Fix for last commit

--- application/models/User/PasswordChangeRequest.php
+++ application/models/User/PasswordChangeRequest.php
@@ -31,7 +31,7 @@ class User_PasswordChangeRequest extends Record
 				$mail = new Mailer();
 				$mail->setFrom('support@say.so')
 					->addTo($userEmail->email)
-					->setSubject( . ' | Password Change Request';
+					->setSubject($productName . ' | Password Change Request';
 				$mail->setBodyMultilineText($message);
 				$mail->setBodyHtml($htmlmessage);
 				$mail->send(new Zend_Mail_Transport_Smtp());

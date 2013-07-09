dd7c38bf8e9424870faafd1dda9620fbee6c31bd
sdaicz <sdaicz@users.noreply.github.com>
2013-07-09 20:42:24 +0000

    Add levels 23 to 27 to Machinima Recon economy. test #1115

---
+++ scripts/sql/2013-07-09-01-game_machinima_add_levels.sql
@@ -0,0 +1,6 @@
+INSERT game_level(game_asset_id, ordinal, threshold, name, img_url, img_url_small, description) VALUES
+(57, 23, 725000, 'Level 23', 'http://media.saysollc.com/media/machinima/Level23_B.png', 'http://media.saysollc.com/media/machinima/Level23_S.png', ''),
+(57, 24, 925000, 'Level 24', 'http://media.saysollc.com/media/machinima/Level24_B.png', 'http://media.saysollc.com/media/machinima/Level24_S.png', ''),
+(57, 25, 1025000, 'Level 25', 'http://media.saysollc.com/media/machinima/Level25_B.png', 'http://media.saysollc.com/media/machinima/Level25_S.png', ''),
+(57, 26, 1150000, 'Level 26', 'http://media.saysollc.com/media/machinima/Level26_B.png', 'http://media.saysollc.com/media/machinima/Level26_S.png', ''),
+(57, 27, 1275000, 'Level 27', 'http://media.saysollc.com/media/machinima/Level27_B.png', 'http://media.saysollc.com/media/machinima/Level27_S.png', '');

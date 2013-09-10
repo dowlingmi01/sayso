569a8b2f65418f4e7ea651144165412979d8efdc
hamzakubba <hamza@say.so>
2013-09-10 17:31:13 +0000

    Lots of updates to SSMART docs, including using Heather's new design

--- public/ssmartdocs/docs.js
+++ public/ssmartdocs/docs.js
@@ -12,11 +12,34 @@ var directives = {
 			transclude: true,
 			replace: true,
 			scope: {
+				name: "@",
+				shown: "@"
+			},
+			template: '<div class="section" ng-class="{\'shown\': shown}"><div class="sectiontriangle"></div><h1 ng-click="shown = !shown" class="sectionname">{{name}}</h1><div class="sectioncontents" ng-transclude></div></div>',
+			link: function (scope, element, attrs, actionClassController) {
+				scope.shown = false;
+				scope.toggleShow = function() {
+					scope.shown = !scope.shown;
+				}
+				scope.actionClassName = actionClassController.actionClassName;
+			},
+			controller: function ($attrs) {
+				this.endpointName = $attrs.name;
+			}
+		}
+	},
+
+	"actionclass": function () {
+		return {
+			restrict: "E",
+			transclude: true,
+			replace: true,
+			scope: {
 				name: "@"
 			},
-			template: '<div class="section"><h1 class="section">{{name.capitalizeFirst()}} Class</h1><div ng-transclude></div></div>',
+			template: '<div class="actionclass"><h2 class="actionclass">{{name.capitalizeFirst()}} Class</h2><div ng-transclude></div></div>',
 			controller: function ($attrs) {
-				this.sectionName = $attrs.name;
+				this.actionClassName = $attrs.name;
 			}
 		}
 	},
@@ -32,24 +55,35 @@ var directives = {
 
 	"endpoint": function () {
 		return {
-			require: "^?section",
+			require: "^?actionclass",
 			restrict: "E",
 			transclude: true,
 			replace: true,
 			scope: {
 				name: "@"
 			},
-			template: '<div class="endpoint"><h2 ng-click="toggleShow()" class="endpoint">{{sectionName}}.{{name}}</h2><div ng-show="shown" ng-transclude></div></div>',
-			link: function (scope, element, attrs, sectionController) {
-				scope.shown = false;
-				scope.toggleShow = function() {
-					scope.shown = !scope.shown;
-				}
-				scope.sectionName = sectionController.sectionName;
+			template: '<div class="endpoint" ng-class="{\'shown\': shown}"><div class="endpointtriangle"></div><h2 ng-click="shown = !shown" class="endpointname">{{actionClassName}}.{{name}}</h2><div class="endpointcontents" ng-transclude></div></div>',
+			link: function (scope, element, attrs, actionClassController) {
+				scope.actionClassName = actionClassController.actionClassName;
 			},
 			controller: function ($attrs) {
 				this.endpointName = $attrs.name;
 			}
+		}
+	},
+
+	"parametersnodefault": function () {
+		return {
+			restrict: "E",
+			transclude: true,
+			replace: true,
+			template: '' +
+				'<div class="parameterscontainer">' +
+					'<div faketable class="parameters" ng-transclude>' +
+						'<parameterheaders></parameterheaders>' +
+					'</div>' +
+					'<div class="notes"><span required>█</span> denotes a required field</div>' +
+				'</div>'
 		}
 	},
 
@@ -63,10 +97,28 @@ var directives = {
 					'<h3>Parameters</h3>' +
 					'<div faketable class="parameters" ng-transclude>' +
 						'<parameterheaders></parameterheaders>' +
-						'<parameter required name="session_id" type="integer" default_value="">The session id.</parameter>' +
-						'<parameter required name="session_key" type="string" default_value="">The session key.</parameter>' +
-						'<parameter required name="action_class" type="string" default_value="">The class the action is in.</parameter>' +
-						'<parameter required name="action" type="string" default_value="">The action/endpoint to be called.</parameter>' +
+						'<parameter required name="session_id" type="integer" default="">The session id.</parameter>' +
+						'<parameter required name="session_key" type="string" default="">The session key.</parameter>' +
+						'<parameter required name="action_class" type="string" default="">The class the action is in.</parameter>' +
+						'<parameter required name="action" type="string" default="">The action/endpoint to be called.</parameter>' +
+					'</div>' +
+					'<div class="notes"><span required>█</span> denotes a required field</div>' +
+				'</div>'
+		}
+	},
+
+	"parameterspublic": function () {
+		return {
+			restrict: "E",
+			transclude: true,
+			replace: true,
+			template: '' +
+				'<div class="parameterscontainer">' +
+					'<h3>Parameters</h3>' +
+					'<div faketable class="parameters" ng-transclude>' +
+						'<parameterheaders></parameterheaders>' +
+						'<parameter required name="action_class" type="string" default="">The class the action is in.</parameter>' +
+						'<parameter required name="action" type="string" default="">The action/endpoint to be called.</parameter>' +
 					'</div>' +
 					'<div class="notes"><span required>█</span> denotes a required field</div>' +
 				'</div>'
@@ -89,9 +141,9 @@ var directives = {
 			scope: {
 				name: "@",
 				type: "@",
-				default_value: "@"
+				default: "@"
 			},
-			template: '<div fakerow class="parameter"><div>{{name}}</div><div>{{type}}</div><div>{{default_value}}</div><div ng-transclude></div></div>'
+			template: '<div fakerow class="parameter"><div>{{name}}</div><div>{{type}}</div><div>{{default}}</div><div ng-transclude></div></div>'
 		}
 	},
 
@@ -114,7 +166,7 @@ var directives = {
 		return {
 			restrict: "E",
 			replace: true,
-			template: '<div fakeheaderrow class="returnvalueheaders"><div>Name</div><div>Type</div><div>Location</div><div>Default Value</div><div>Description</div></div>'
+			template: '<div fakeheaderrow class="returnvalueheaders"><div>Name</div><div>Type</div><div>Location</div><div>Description</div></div>'
 		}
 	},
 
@@ -126,10 +178,9 @@ var directives = {
 			scope: {
 				name: "@",
 				type: "@",
-				location: "@",
-				default_value: "@"
+				location: "@"
 			},
-			template: '<div fakerow class="returnvalue"><div>{{name}}</div><div>{{type}}</div><div>{{location}}</div><div>{{default_value}}</div><div ng-transclude></div></div>'
+			template: '<div fakerow class="returnvalue"><div>{{name}}</div><div>{{type}}</div><div>{{location}}</div><div ng-transclude></div></div>'
 		}
 	},
 
@@ -142,7 +193,7 @@ var directives = {
 				name: "@"
 			},
 			template: '<div class="example"><h3 ng-click="toggleShow()" class="example">{{name}}</h2><div ng-show="shown" ng-transclude></div></div>',
-			link: function (scope, element, attrs, sectionController) {
+			link: function (scope, element, attrs, actionClassController) {
 				scope.shown = false;
 				scope.toggleShow = function() {
 					scope.shown = !scope.shown;
@@ -207,19 +258,19 @@ var directives = {
 
 	"requestexample": function () { // "requestexample" is kinda a hack, because it doesn't use <example> and <json> etc.
 		return {
-			require: ["^?section", "^?endpoint"],
+			require: ["^?actionclass", "^?endpoint"],
 			restrict: "E",
 			transclude: true,
 			replace: false,
 			template: '' +
 				'<div class="example">' +
-					'<h3 ng-click="toggleShow()" class="example">Request Example - json</h3>' +
+					'<h3 ng-click="toggleShow()" class="example">Request Example</h3>' +
 					'<div class="json" ng-show="shown">' +
 						'<div>' +
 							'<div class="nodecontainer">{<div class="node" ng-transclude>' +
 								'<variable required name="session_id">20324</variable>' +
 								'<variable required name="session_key">2SVHO2VB997FBWYTARRCPMF4SLA2NQJ6</variable>' +
-								'<variable required name="action_class">{{sectionName}}</variable>' +
+								'<variable required name="action_class">{{actionClassName}}</variable>' +
 								'<variable required name="action">{{endpointName}}</variable>' +
 							'</div>}</div>' +
 						'</div>' +
@@ -231,7 +282,37 @@ var directives = {
 				scope.toggleShow = function() {
 					scope.shown = !scope.shown;
 				}
-				scope.sectionName = controllers[0].sectionName;
+				scope.actionClassName = controllers[0].actionClassName;
+				scope.endpointName = controllers[1].endpointName;
+			}
+		}
+	},
+
+	"requestexamplepublic": function () { // "requestexample" is kinda a hack, because it doesn't use <example> and <json> etc.
+		return {
+			require: ["^?actionclass", "^?endpoint"],
+			restrict: "E",
+			transclude: true,
+			replace: false,
+			template: '' +
+				'<div class="example">' +
+					'<h3 ng-click="toggleShow()" class="example">Request Example</h3>' +
+					'<div class="json" ng-show="shown">' +
+						'<div>' +
+							'<div class="nodecontainer">{<div class="node" ng-transclude>' +
+								'<variable required name="action_class">{{actionClassName}}</variable>' +
+								'<variable required name="action">{{endpointName}}</variable>' +
+							'</div>}</div>' +
+						'</div>' +
+					'</div>' +
+				'</div>' +
+			'',
+			link: function (scope, element, attrs, controllers) {
+				scope.shown = false;
+				scope.toggleShow = function() {
+					scope.shown = !scope.shown;
+				}
+				scope.actionClassName = controllers[0].actionClassName;
 				scope.endpointName = controllers[1].endpointName;
 			}
 		}
@@ -239,7 +320,7 @@ var directives = {
 
 	"commonvariables": function () {
 		return {
-			require: ["^?section", "^?endpoint"],
+			require: ["^?actionclass", "^?endpoint"],
 			restrict: "E",
 			transclude: true,
 			replace: true,
@@ -249,11 +330,11 @@ var directives = {
 			template: '<div>' +
 					'<variable required name="session_id">20324</variable>' +
 					'<variable required name="session_key">2SVHO2VB997FBWYTARRCPMF4SLA2NQJ6</variable>' +
-					'<variable required name="action_class">{{sectionName}}</variable>' +
+					'<variable required name="action_class">{{actionClassName}}</variable>' +
 					'<variable required name="action">{{endpointName}}</variable>' +
 			'</div>',
 			link: function (scope, element, attrs, controllers) {
-				scope.sectionName = controllers[0].sectionName;
+				scope.actionClassName = controllers[0].actionClassName;
 				scope.endpointName = controllers[1].endpointName;
 			}
 		}

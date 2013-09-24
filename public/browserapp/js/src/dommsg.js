f9f28086cfc0e9b911e950b4a08a4497d01de02a
sdaicz <sdaicz@users.noreply.github.com>
2013-09-24 00:14:37 +0000

    Don't mask exceptions in handler functions for messages.

--- public/browserapp/js/src/dommsg.js
+++ public/browserapp/js/src/dommsg.js
@@ -1,17 +1,18 @@
 sayso.module.dommsg = (function(global, util) {
 	var handlers = {};
 	function handleMessage( event ) {
+		var data;
 		try {
-			var data = JSON.parse(event.data);
+			data = JSON.parse(event.data);
 			if( typeof data === 'string' )
 				data = JSON.parse(data);
-
-			var prefix = 'sayso-';
-			var evt;
-			if( data[0] && data[0].slice && data[0].slice(0, prefix.length) === prefix && handlers[(evt=data[0].slice(prefix.length))])
-				handlers[evt](data[1], event.source);
 		} catch( e ) {
 		}
+
+		var prefix = 'sayso-';
+		var evt;
+		if( data && data[0] && data[0].slice && data[0].slice(0, prefix.length) === prefix && handlers[(evt=data[0].slice(prefix.length))])
+			handlers[evt](data[1], event.source);
 	}
 	function addHandler( name, fn ) {
 		handlers[name] = fn;

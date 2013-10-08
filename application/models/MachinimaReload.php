5cfc6972314f4b054e194890d819d5e0d160d90f
sdaicz <sdaicz@users.noreply.github.com>
2013-10-08 21:04:10 +0000

    Add new client_id parameter for Machinima user validation. re #1139

--- application/models/MachinimaReload.php
+++ application/models/MachinimaReload.php
@@ -2,8 +2,8 @@
 
 class MachinimaReload {
 	public static function verifyEmail( $email, $digest ) {
-		$url = sprintf('http://%s/ams_profile?email_id=%s&digest=%s',
-			Registry::getConfig()->machinimareload->domain, $email, $digest);
+		$url = sprintf('http://%s/ams_profile?email_id=%s&digest=%s&client_id=%s',
+			Registry::getConfig()->machinimareload->domain, $email, $digest, Registry::getConfig()->machinimareload->client_id);
 		$client = new Zend_Http_Client($url);
 		$response = $client->request();
 		if($response->isSuccessful()) {
@@ -12,6 +12,7 @@ class MachinimaReload {
 			if( is_array($value) && array_key_exists('username', $value) ) {
 				$value['machinimareload_digest'] = $digest;
 				$value['birthdate'] = $value['birthday'];
+				$value['username'] = $value['first_name'] . ' ' . $value['last_name'];
 				return($value);
 			}
 		}
